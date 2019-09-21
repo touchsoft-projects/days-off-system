2352e60e7eadd63b704938aed0cf07e68b4f5bc8
Alexandr <hodukovaleksandr@yandex.ru>
2019-09-21 16:27:18 +0300

    Add Spring and Swagger conf

---
+++ src/main/java/com/touchsoft/rest/config/AppConfig.java
@@ -0,0 +1,10 @@
+package com.touchsoft.rest.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan("com.touchsoft")
+@Configuration
+public class AppConfig {
+
+}
---
+++ src/main/java/com/touchsoft/rest/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.touchsoft.rest.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.touchsoft.rest.controllers")
+public class WebConfig implements WebMvcConfigurer {
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		 registry.addResourceHandler("swagger-ui.html")
+	      .addResourceLocations("classpath:/META-INF/resources/");
+	 
+	    registry.addResourceHandler("/webjars/**")
+	      .addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+	
+}
