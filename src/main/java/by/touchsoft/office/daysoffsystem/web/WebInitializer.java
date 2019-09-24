de18f648c86096ea4d97d6ac2174ba46b2c08797
Alexandr <hodukovaleksandr@yandex.ru>
2019-09-24 18:33:12 +0300

    TASK-1: created base of application.

---
+++ src/main/java/by/touchsoft/office/daysoffsystem/web/WebInitializer.java
@@ -0,0 +1,36 @@
+package by.touchsoft.office.daysoffsystem.web;
+
+import by.touchsoft.office.daysoffsystem.web.config.AppConfig;
+import by.touchsoft.office.daysoffsystem.web.config.SwaggerConfig;
+import by.touchsoft.office.daysoffsystem.web.config.WebConfig;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+import java.nio.charset.StandardCharsets;
+
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{AppConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[]{WebConfig.class, SwaggerConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding(StandardCharsets.UTF_8.name());
+        characterEncodingFilter.setForceEncoding(true);
+        return new Filter[]{characterEncodingFilter};
+    }
+
+}
---
+++ src/test/java/by/touchsoft/office/daysoffsystem/web/controllers/TestController.java
@@ -0,0 +1,25 @@
+package by.touchsoft.office.daysoffsystem.web.controllers;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class TestController {
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(new Controller()).build();
+    }
+
+    @Test
+    public void testDaysoffTestResultOK() throws Exception {
+        this.mockMvc.perform(get("/daysoff/test"))
+                .andExpect(status().isOk());
+    }
+}
